9a55d842c49f41f30f55203f7ba1d1a694e3b3a4
Abtahi chowdhury <abtahejisan@gmail.com>
2025-10-01 18:33:19 +0600

    Cartviewset updated

--- order/views.py
+++ order/views.py
@@ -8,6 +8,7 @@ from order import serializers as orderSz
 from rest_framework.decorators import action
 from order.services import OrderService
 from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
@@ -21,7 +22,14 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
         if getattr(self, 'swagger_fake_view', False):
             return Cart.objects.none()
         return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
-
+    
+    def create(self, request, *args, **kwargs):
+        existing_Cart=Cart.objects.filter(user=self.request.user).first()
+        if existing_Cart:
+            serializer=self.get_serializer(existing_Cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return super().create(request,*args,**kwargs)
+        
 class CartItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
 
